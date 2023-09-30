866860e98d26172bcac94d513731aee41a9f3877
Chornbeek <125606362+Chornbeek@users.noreply.github.com>
2023-09-29 21:36:33 -0400

    Update MagicMaze.java

--- MagicMaze.java
+++ MagicMaze.java
@@ -1,23 +1,29 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-
 /*
- * 
-
- STARTING BOTTOM LEFT CORNER, X IS GOAL
+ * 	Maze 1:
   	*******@@@****X
 	@****@@@@@@@***
 	@*************1
  	*@@@@@@@*******
-	1******@@@@@@@@
+	1*******@@@@@@@
  	****@**********
  	****@****@*****
  	****@****@*****
  	****@****@*****
  	****@****@*****
  	*********@*****
+
+  	Maze 2:
+   	2***@8**X@*****
+	****@@@@@@@@@@@
+	*3**@3**4@****5
+	@@@@@@@@@@@@@@@
+	****@***6@*****
+	****@5***@****7
+	6**7@***4@**8**
+	@@@@@@@@@@@@@@@
+	***1@****@****1
+	****@****@*****
+	****@****@2****
  	
  	The digits such as ‘1’, ‘2’, ‘3’, etc… represent magical squares. These squares will
 teleport you to other matching digit in the maze. For example, the ‘1’ in the above figure,
@@ -26,19 +32,13 @@ digit spots.
  	
  */
 
-
 public class MagicMaze {
     public char[][] maze;     // 2D array to store the maze
     public String mazeName;  // Name of the maze (e.g., "maze1")
-    public static int numRows;      // Number of rows in the maze
-    public static int numCols;      // Number of columns in the maze
+    public int numRows;      // Number of rows in the maze
+    public int numCols;      // Number of columns in the maze
+    Map<Character, int[]> numberMap = new HashMap<>();
     
-    public static final char WALL = '@';
-    public static final char EXIT = 'X';
-    public static final char VISITED = '.';
-    public static final char PATH = '*';
-    
-  
 
     // Constructor to initialize the MagicMaze object
     public MagicMaze(String mazeName, int rows, int cols) throws IOException {
@@ -47,8 +47,8 @@ public class MagicMaze {
         maze = new char[numRows][numCols];
         
         
-        
         readMazeFromFile(mazeName);
+        createNumberMap();
     }
     
     
@@ -111,26 +111,67 @@ public class MagicMaze {
         
     }
     
+    
+    private void createNumberMap() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                char currentChar = maze[i][j];
+                if (Character.isDigit(currentChar)) {
+                    // Convert the digit to its integer value
+                    int digit = Character.getNumericValue(currentChar);
+
+                    // Check if the digit is already in the map
+                    if (numberMap.containsKey(currentChar)) {
+                        // Found two identical numbers; print their coordinates
+                        int[] coordinates = numberMap.get(currentChar);
+                        System.out.println("Identical numbers " + currentChar + " found at coordinates (" + coordinates[0] + ", " + coordinates[1] + ") and (" + i + ", " + j + ")");
+                        
+                    } else {
+                        // Store the coordinates of the current digit in the map
+                        numberMap.put(currentChar, new int[]{i, j});
+                    }
+            }
+        }
+    }
+    }
 
     // Recursive backtracking function to explore maze paths
     public boolean solveMazeR(char[][] maze, int x, int y,  char[][] sol) {
-    	
+    	//System.out.println("Current Position: (" + x + ", " + y + ")");
     	// if (x, y is goal) return true
         if (maze[x][y] == 'X') {
             sol[x][y] = '.';
             return true;
+            
         }
-
+        
+        
+        
         // Check if maze[x][y] is valid
         if (isSafe(maze, x, y) == true) {
             // Check if the current block is already part of solution path.
             if (sol[x][y] == '.')
                 return false;
             
+            if (Character.isDigit(maze[x][y])) {
+                char num = maze[x][y];
+                if (numberMap.containsKey(num)) {
+                    int[] teleport = numberMap.get(num);
+                    int newX = teleport[0];
+                    int newY = teleport[1];
+                    System.out.println("Teleporting from (" + x + ", " + y + ") to (" + newX + ", " + newY + ")");
+                    x = newX;
+                    y = newY;
+                    
+                }
+            }
             
-
+            
+            
+            
             // mark x, y as part of solution path
             sol[x][y] = '.';
+            
 
             /* Move forward in x direction */
             if (x + 1 < numRows && solveMazeR(maze, x + 1, y, sol))
@@ -156,7 +197,7 @@ public class MagicMaze {
             sol[x][y] = ' ';
             return false;
         }
-
+        
         return false;
     }
     
@@ -165,14 +206,15 @@ public class MagicMaze {
     boolean isSafe( char[][] maze, int x, int y)
 	{
 		// if (x, y outside maze) return false
-		return (x >= 0 && x < numRows && y >= 0 && y < numCols && maze[x][y] == '*');  //Maybe - 1
+		return (x >= 0 && x < numRows && y >= 0 && y < numCols && maze[x][y] != '@');  //Maybe - 1
 	}
 
     // Main method (for testing purposes)
     public static void main(String[] args) throws IOException {
-    	 MagicMaze maze = new MagicMaze("maze1.txt", numRows, numCols);
+    	 MagicMaze maze = new MagicMaze("maze2.txt", 11, 15);
+    	 
          try {
-             maze.readMazeFromFile("maze1.txt");
+             maze.readMazeFromFile("maze2.txt");
              boolean solved = maze.solveMagicMaze();
              if (solved) {
                  System.out.println("Maze solved!");
@@ -184,10 +226,6 @@ public class MagicMaze {
          }
      }
     }
-
-
-   
-
 
 
 
